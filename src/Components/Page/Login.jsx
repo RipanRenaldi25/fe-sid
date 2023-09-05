7ced4fb057f39017a57cb7d27e0eee0aa494b8a4
ripanrenaldi25 <ripanrenaldi25042002@gmail.com>
2023-09-06 02:51:47 +0700

    style(login page): Responsive login page

--- src/Components/Page/Login.jsx
+++ src/Components/Page/Login.jsx
@@ -5,14 +5,14 @@ import LoginForm from '../Presentational/LoginForm';
 
 function Login() {
   return (
-    <article className="px-10 py-5 flex">
+    <article className="mobile:flex-col mobile:gap-4 px-10 py-5 flex md:flex-row ">
       <section className="flex-1 text-white">
-        <div className="py-10 px-8 w-[57%] bg-gradient-to-tr from-primary-black to-secondary-black mx-auto min-h-[calc(70vh-100px)] rounded-xl">
+        <div className="py-10 px-8 md:w-[57%] bg-gradient-to-tr from-primary-black to-secondary-black mx-auto min-h-[calc(70vh-100px)] rounded-xl">
           <img src={loginAnimation} alt="login-animation" />
         </div>
       </section>
       <section className="flex-1">
-        <div className="w-[70%] border-2 min-h-[calc(75vh-100px)] px-4 py-10 rounded-lg">
+        <div className="md:w-[70%] border-2 min-h-[calc(75vh-100px)] px-4 py-10 rounded-lg">
           <header className="flex justify-center">
             <span className="text-4xl"><PiUsersThreeDuotone /></span>
           </header>
